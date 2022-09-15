32605c393f52e8b3c245bf4edb3bcd02184c33b1
BRM6 <madimadi88@icloud.com>
2022-09-15 14:21:32 -0400

    contact manager test

---
+++ src/main/java/com/programming/techie/ContactManagerTest.java
@@ -0,0 +1,16 @@
+package com.programming.techie;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactManagerTest {
+
+    @Test
+    public void test(){
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("DI", "MA", "0343988128");
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+    }
+}
